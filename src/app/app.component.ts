907d085b64f06ecd98aae7e9c68d88b0b49969a2
lokinindi18 <bhargav.prasad@gmail.com>
2017-10-30 20:04:28 -0400

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  myPost = {
+    title: 'MyPostTitle',
+    isFavorite: false
+  }
+
+  someDummyMethod() {
+    console.log('Hello !!!' + this.title);
+  }
 }
